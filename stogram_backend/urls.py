2a94471ac8f60a76ee845d82c0bffd6a590b54c1
yuhim01@gmail.com <yuhim01@gmail.com>
2023-07-16 19:41:14 +0300

    Add auth

--- stogram_backend/urls.py
+++ stogram_backend/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from stogram_app import views
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path('api/user/<int:user_id>', views.UserInfo.as_view()),
     path('api/user_posts/<int:id>', views.UserPosts.as_view()),
     path('api/votes/<int:post_id>', views.PostVotes.as_view()),
+    path('api-auth/', include('rest_framework.urls')),
 ]
